83cf11685f3937666d050b570f7054f795229f2b
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-06-24 19:52:39 +0400

    Fix mapping reference in futex.c

--- kernel/futex.c
+++ kernel/futex.c
@@ -288,7 +288,7 @@ get_futex_key(u32 __user *uaddr, int fshared, union futex_key *key, int rw)
 	unsigned long address = (unsigned long)uaddr;
 	struct mm_struct *mm = current->mm;
 	struct page *page, *page_head;
-	struct address_space *mapping = READ_ONCE(page->mapping);
+	struct address_space *mapping = READ_ONCE(page_head->mapping);
 	int err, ro = 0;
 
 	/*
