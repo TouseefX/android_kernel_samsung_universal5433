50c733c6e7af2f1f24a67c1889eb29834f2f2ab9
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-06-24 15:27:21 +0000

    fix fs.h file

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1117,7 +1117,7 @@ extern int vfs_test_lock(struct file *, struct file_lock *);
 extern int vfs_lock_file(struct file *, unsigned int, struct file_lock *, struct file_lock *);
 extern int vfs_cancel_lock(struct file *filp, struct file_lock *fl);
 extern int flock_lock_file_wait(struct file *filp, struct file_lock *fl);
-extern int __break_lease(struct inode *inode, unsigned int flags);
+extern int __break_lease(struct inode *inode, unsigned int flags, unsigned int type);
 extern void lease_get_mtime(struct inode *, struct timespec *time);
 extern int generic_setlease(struct file *, long, struct file_lock **);
 extern int vfs_setlease(struct file *, long, struct file_lock **);
@@ -1226,7 +1226,7 @@ static inline int flock_lock_file_wait(struct file *filp,
 	return -ENOLCK;
 }
 
-static inline int __break_lease(struct inode *inode, unsigned int mode)
+static inline int __break_lease(struct inode *inode, unsigned int mode, unsigned int type)
 {
 	return 0;
 }
@@ -2088,7 +2088,7 @@ static inline int locks_verify_truncate(struct inode *inode,
 static inline int break_lease(struct inode *inode, unsigned int mode)
 {
 	if (inode->i_flock)
-		return __break_lease(inode, mode);
+		return __break_lease(inode, mode, FL_LEASE);
 	return 0;
 }
 
@@ -2175,7 +2175,6 @@ static inline int break_deleg_wait(struct inode **delegated_inode)
 	BUG();
 	return 0;
 }
-
 #endif /* CONFIG_FILE_LOCKING */
 
 /* fs/open.c */
