fff0dcf4e754bbc40a890073ba9be08e9493fbee
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-07-01 02:36:44 +0400

    Add pte_mkspecial function to pgtable.h

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -298,6 +298,8 @@ static inline pte_t pte_mknexec(pte_t pte)
 	return set_pte_bit(pte, __pgprot(L_PTE_XN));
 }
 
+static inline pte_t pte_mkspecial(pte_t pte) { return pte; }
+
 static inline pte_t pte_modify(pte_t pte, pgprot_t newprot)
 {
 	const pteval_t mask = L_PTE_XN | L_PTE_RDONLY | L_PTE_USER |
