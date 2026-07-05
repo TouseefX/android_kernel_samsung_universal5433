d61c49d0b5a4bd0840558d257db48d3cb2bde35d
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-07-05 08:29:24 +0000

    Revert "mm: tail page refcounting optimization for slab and hugetlbfs"
    This reverts commit 19426c27e01294ec0c4d2143f794ab96b20db9ac.

--- mm/internal.h
+++ mm/internal.h
@@ -56,8 +56,7 @@ static inline void __get_page_tail_foll(struct page *page,
 	VM_BUG_ON(page_mapcount(page) < 0);
 	if (get_page_head)
 		atomic_inc(&page->first_page->_count);
-	if (compound_tail_refcounted(page->first_page))
-		atomic_inc(&page->_mapcount);
+	atomic_inc(&page->_mapcount);
 }
 
 /*
